bc131a57fc4db2c9e7ddf9e8d3b732d7648df7c8
Aluminium51 <noptanadon.p@gmail.com>
2025-06-16 14:35:11 +0700

    add vercel analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { Menu, X, Github, Mail, Instagram, Facebook, ExternalLink, Code, Shield, CheckCircle, Zap } from 'lucide-react';
 import Footer from './components/Footer';
 import Contact from './components/sections/Contact';
 import Navigation from './components/Navbar';
@@ -8,7 +7,6 @@ import HeroSection from './components/sections/Hero';
 import ProjectsSection from './components/sections/Projects';
 import SkillsSection from './components/sections/Skills';
 import AboutSection from './components/sections/About';
-import FadeInSection from './components/FadeInSection';
 import StarryBackground from './components/StarryBackground';
 
 
